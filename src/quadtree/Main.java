53f09703199451c343e48e74ab88cbb01b223954
kiwi-fruitiw <zane.tian@gmail.com>
2021-08-31 11:27:11 -0700

    Add FPS counter, Particle, mouseDragged point adder

--- src/quadtree/Main.java
+++ src/quadtree/Main.java
@@ -1,21 +1,35 @@
 package quadtree;
 
 import processing.core.PApplet;
+import processing.core.PConstants;
+import processing.core.PFont;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.IntStream;
 
+/**
+ * This is a Java port of my Python port of Daniel Shiffman's Quadtree
+ * Collisions Coding Challenge #98.3 https://www.youtube.com/watch?v=z0YFFg_nBjw
+ *
+ * @author Kiwi
+ * @date 2021.08.31
+ */
 public class Main extends PApplet {
-	// generate points, draw points on mouse drag
-	// make quadtree
-	// .insert(), .subdivide()
-	// .query()
-	// query box with mouse hover
-	// add collision detection » point.intersects
-	// use quadtree in detection
+	/*
+	 make particles, .move(), .render()
+	 generate particles, draw points on mouse drag
 
-	List<Point> points;
+	 TODO make Rectangle, Point
+	 TODO make Quadtree
+	 TODO .insert(), .subdivide()
+	 TODO .query()
+	 TODO query box with mouse hover
+	 TODO add collision detection » point.intersects
+	 TODO use quadtree in collision detection
+	*/
+
+	List<Particle> particles;
 
 	public static void main(String[] args) {
 		PApplet.main(new String[] {Main.class.getName()});
@@ -32,10 +46,10 @@ public class Main extends PApplet {
 		frameRate(144);
 		noStroke();
 
-		points = new ArrayList<>();
+		particles = new ArrayList<>();
 
-		IntStream.range(0, 400).forEach(i -> points.add(
-				new Point(random(width), random(height))
+		IntStream.range(0, 400).forEach(i -> particles.add(
+				new Particle(this, random(width), random(height))
 		));
 
 	}
@@ -44,8 +58,30 @@ public class Main extends PApplet {
 	public void draw() {
 		background(210, 80, 40);
 
-		for (Point point : points) {
-			circle(point.x, point.y, 10);
+		for (Particle particle : particles) {
+			particle.move(this);
 		}
+
+		for (Particle particle : particles) {
+			particle.render(this);
+		}
+
+		display_FPS();
+	}
+
+	@Override
+	public void mouseDragged() {
+		particles.add(new Particle(this, mouseX, mouseY));
+	}
+
+	private void display_FPS() {
+		PFont mono = createFont("data/terminus.ttf", 24);
+		textFont(mono);
+		String s = String.format("FPS: %.0f", frameRate);
+		int pad = 5;
+		fill(0, 0, 100, 50);
+		rect(12-pad, 24+pad, textWidth(s)+10, -25, 3);
+		fill(0, 0, 100, 100);
+		text(s, 12, 24);
 	}
 }
--- src/quadtree/Particle.java
+++ src/quadtree/Particle.java
@@ -1,5 +1,7 @@
 package quadtree;
 
+import processing.core.PApplet;
+
 /**
  * A particle has (x,y) coordinates and can jitter around the canvas with its
  * own show / render method.
@@ -8,7 +10,21 @@ public class Particle {
 	float x, y;
 	int r;
 
-	public Particle() {
+	public Particle(PApplet self, float x, float y) {
+		this.x = x;
+		this.y = y;
 
+		this.r = 4;
+	}
+
+	public void move(PApplet self) {
+		x += self.random(-1, 1);
+		y += self.random(-1, 1);
+	}
+
+	public void render(PApplet self) {
+		self.noStroke();
+		self.fill(0, 0, 100);
+		self.circle(x, y, r*2);
 	}
 }
