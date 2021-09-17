31b4cf79df4a0ee7dba8caf8e555a4d4d6ab393b
kiwi-fruitiwi <zane.tian@gmail.com>
2021-09-17 11:13:23 -0700

    Add gravity test

--- src/gravitation/Main.java
+++ src/gravitation/Main.java
@@ -1,14 +1,21 @@
 package gravitation;
 
 import processing.core.PApplet;
+import processing.core.PVector;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Thanks to our hero Abe Pazos at https://vimeo.com/channels/p5idea, who
  * teaches us how to use Processing inside IDEA
  */
 public class Main extends PApplet {
+    List<Planet> planets;
+    PVector gravity;
+
     public static void main(String[] args) {
-        PApplet.main(new String[]{example.Main.class.getName()});
+        PApplet.main(new String[]{gravitation.Main.class.getName()});
     }
 
     @Override
@@ -20,12 +27,25 @@ public class Main extends PApplet {
     public void setup() {
         rectMode(RADIUS);
         colorMode(HSB, 360f, 100f, 100f, 100f);
+
+        gravity = new PVector(0, 0.1f);
+        planets = new ArrayList<>();
+        for (int i=0; i < 1000; i++) {
+            planets.add(new Planet(this, (int) random(width),
+                    (int) random(height),
+                    (int) random(20)+1));
+        }
     }
 
     @Override
     public void draw() {
         background(210, 100, 30, 100);
-        rect(mouseX, mouseY, 20, 20);
+
+        for (Planet p : planets) {
+            p.update(this);
+            p.show(this);
+            p.apply_force(this, gravity);
+        }
     }
 
     @Override
--- src/gravitation/Planet.java
+++ src/gravitation/Planet.java
@@ -1,7 +1,6 @@
 package gravitation;
 
 import processing.core.PApplet;
-import processing.core.PConstants;
 import processing.core.PVector;
 
 public class Planet {
@@ -13,14 +12,14 @@ public class Planet {
 
     public Planet(PApplet app, int x, int y, int mass) {
         pos = new PVector(x , y);
-        vel = PVector.random2D().mult(5);
+        vel = PVector.random2D().mult(0);
         acc = new PVector(0, 0);
         this.mass = mass;
         r = (float) Math.sqrt(mass) * 2;
     }
 
     public void show(PApplet app) {
-        app.fill(0, 0, 100, 20);
+        app.fill(0, 0, 100, 20); // white
         app.circle(pos.x, pos.y, r*2);
     }
 
